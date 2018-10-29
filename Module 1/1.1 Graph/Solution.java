d573591736a60f56a4bd0147f880a89fd2964a1f
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-29 21:36:25 +0530

    "ads2_m1_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 1/1.1 Graph/Solution.java
+++ Module 1/1.1 Graph/Solution.java
@@ -6,32 +6,71 @@ interface Graph {
     // public Iterable<Integer> adj(int v);
     public boolean hasEdge(int v, int w);
 }
+/**
+ * Class for graph class.
+ */
 
 class GraphClass implements Graph {
+	/**
+	 * number of vertices.
+	 */
 	private int V;
+	/**
+	 * number of edges.
+	 */
 	private int E;
+	/**
+	 * adj array of bag class type.
+	 */
 	private Bag<Integer>[] adj;
+
+	/**
+	 * Gets the e.
+	 *
+	 * @return     The e of int type.
+	 */
 
 	public int getE() {
         return E;
 	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      V     { number of vertices }
+	 */
 
 	public GraphClass(int V) {
 		this.V = V;
-		// this.E = E;
 		adj = (Bag<Integer>[]) new Bag[V];
 		for (int v = 0; v < V; v++) {
             adj[v] = new Bag<Integer>();
         }
 	}
+	/**
+	 * number of integers.
+	 *
+	 * @return     { int }
+	 */
 
 	public int V() {
         return V;
     }
 
+    /**
+     * number of vertices.
+     *
+     * @return     { int }
+     */
+
     public int E() {
         return E;
     }
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { vertex }
+     * @param      w     { edge }
+     */
 
     public void addEdge(int v, int w) {
     	if(v == w) {
@@ -44,17 +83,32 @@ class GraphClass implements Graph {
         adj[v].add(w);
         adj[w].add(v);
     }
+    /**
+     * determines if vertices are connected or not.
+     *
+     * @param      v     { vertex. }
+     * @param      w     { edge }
+     *
+     * @return     True if has edge, False otherwise.
+     */
 
     public boolean hasEdge(int v, int w) {
-    	if(adj[v].contains(w))
+    	if(adj[v].contains(w)) {
     		return true;
-    	return false;
+    	} else {
+    	    return false;
+    	}
     }
+    /**
+     * prints the list of cities connected to other cities.
+     *
+     * @param      cities  The cities
+     *
+     * @return     { description_of_the_return_value }
+     */
 
     public String printList(String[] cities) {
         StringBuilder s = new StringBuilder();
-        // s.append(V + " vertices, " + E + " edges " +"\n");
-
             for (int v = 0; v < V; v++) {
             s.append(cities[v] + ": ");
             for (int w : adj[v]) {
@@ -64,12 +118,24 @@ class GraphClass implements Graph {
         }
         return s.toString();
     }
-
-
 }
+/**
+ * Class for solution.
+ */
 
-class Solution {
-	public static void main(String[] args) {
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
 		String api = input.nextLine();
 		int vertex = Integer.parseInt(input.nextLine());
@@ -79,11 +145,11 @@ class Solution {
 		String[] cities = input.nextLine().split(",");
 		GraphClass gc = new GraphClass(vertex);
 		// System.out.println(gc);
-		for(int i = 0; i<edge; i++) {
+		for (int i = 0; i < edge; i++) {
 			String[] tokens = input.nextLine().split(" ");
 			gc.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 		}
-		System.out.println(vertex+" vertices, "+gc.getE()+" edges");
+		System.out.println(vertex + " vertices, " + gc.getE() + " edges");
 
 		if (edge == 0 || edge == 1 || vertex == 1) {
 			System.out.println("No edges");
@@ -91,12 +157,11 @@ class Solution {
 		}
 		switch(api) {
 			case "Matrix":
-			for(int i = 0; i<vertex; i++) {
-				for(int j = 0; j< vertex; j++) {
-					if(gc.hasEdge(i, j)) {
+			for (int i = 0; i < vertex; i++) {
+				for (int j = 0; j < vertex; j++) {
+					if (gc.hasEdge(i, j)) {
 					    System.out.print("1 ");
-					}
-					else {
+					} else {
 						System.out.print("0 ");
 					}
 				}
@@ -106,8 +171,9 @@ class Solution {
 			break;
 			case "List":
 		    System.out.println(gc.printList(cities));
-
-
+		    break;
+		    default:
+		    break;
 		}
 	}
 }
