4f8cb47748af8f72caee48fa64fb9b42ffb11741
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-29 19:10:16 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 1/1.1 Graph/Solution.java
+++ Module 1/1.1 Graph/Solution.java
@@ -1,8 +1,89 @@
+import java.util.Scanner;
 interface Graph {
     public int V();
     public int E();
     public void addEdge(int v, int w);
-    public Iterable<Integer> adj(int v);
+    // public Iterable<Integer> adj(int v);
     public boolean hasEdge(int v, int w);
 }
 
+class GraphClass implements Graph {
+	private int V;
+	private int E;
+	private Bag<Integer>[] adj;
+
+	public GraphClass(int V, int E) {
+		this.V = V;
+		this.E = E;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+	}
+
+	public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    public boolean hasEdge(int v, int w) {
+    	if(adj[v].contains(w))
+    		return true;
+    	return false;
+    }
+
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String api = input.nextLine();
+		int vertex = Integer.parseInt(input.nextLine());
+		int edge = Integer.parseInt(input.nextLine());
+		String[] cities = input.nextLine().split(",");
+		GraphClass gc = new GraphClass(vertex, edge);
+		for(int i = 0; i<edge; i++) {
+			String[] tokens = input.nextLine().split(" ");
+			gc.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+		}
+		switch(api) {
+			case "Matrix":
+			System.out.println(vertex+" vertices,"+edge+" edges");
+			for(int i = 0; i<vertex; i++) {
+				for(int j = 0; j<edge; j++) {
+					if(gc.hasEdge(i, j)) {
+						if(j == edge-1) {
+						    System.out.print("1");
+						}
+					    else {
+					    	System.out.print("1 ");
+					    }
+					}
+					else {
+						if(j == edge-1) {
+						    System.out.print("0");
+						}
+					    else {
+					    	System.out.print("0 ");
+					    }
+					}
+				}
+				if(i != vertex-1)
+				System.out.println();
+			}
+
+
+
+		}
+
+	}
+}
