ebfbefb9ccd36729dad19cc6da4d2111facec40d
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-31 15:30:31 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 3/3.1 Directed Cycle/Bag.java
+++ Module 3/3.1 Directed Cycle/Bag.java
@@ -1,85 +1,144 @@
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of
- *  generic items. It supports insertion and iterating over the
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
-public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
 
-    // helper linked list class
+public class Bag<Item> implements Iterable<Item> {
+    /**
+     *  number of elements in bag.
+     */
+    private int num;
+    /**
+     * beginning of bag.
+     */
+    private Node first;
+
+    /**
+     * Class for node.
+     */
+
     private class Node {
+        /**
+         * item of type node.
+         */
         private Item item;
+        /**
+         * Node of type node.
+         */
         private Node next;
     }
 
-   /**
-     * Create an empty stack.
+    /**
+     * Constructs the object.
      */
+
     public Bag() {
         first = null;
-        N = 0;
+        num = 0;
     }
 
-   /**
-     * Is the BAG empty?
+    /**
+     * Determines if empty.
+     * The time complexity is O(1).
+     *
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the bag.
+    /**
+     * returns size of the bag.
+     * The time complexity is O(1).
+     *
+     *
+     * @return     { int }
      */
     public int size() {
-        return N;
+        return num;
     }
 
-   /**
-     * Add the item to the bag.
+    /**
+     * adds element into bag.
+     * The time complexity is O(1).
+     *
+     *
+     * @param      item  The item
      */
-    public void add(Item item) {
+    public void add(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
-        N++;
+        num++;
+    }
+    /**
+     * determines if there is vertex or not.
+     * The time complexity is O(N).
+     *
+     *
+     * @param      item  The item
+     *
+     * @return     { true or false }
+     */
+
+    public boolean contains(final Item item) {
+        Node n = first;
+        while (n != null) {
+            if (n.item == item) {
+                return true;
+            }
+            n = n.next;
+        }
+        return false;
     }
 
 
-   /**
+    /**
      * Return an iterator that iterates over the items in the bag.
+     *
+     *
+     * @return     { iterator }
      */
     public Iterator<Item> iterator()  {
         return new ListIterator();
     }
+    /**
+     * Class for list iterator.
+     */
 
-    // an iterator, doesn't implement remove() since it's optional
     private class ListIterator implements Iterator<Item> {
+        /**
+         * Node.
+         */
         private Node current = first;
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext()  {
+            return current != null;
+        }
+        /**
+         * removes.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * gives next node.
+         *
+         * @return     { item }
+         */
 
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                 throw new NoSuchElementException();
+            }
             Item item = current.item;
             current = current.next;
             return item;
@@ -87,5 +146,4 @@ public class Bag<Item> implements Iterable<Item> {
     }
 
 }
-
 
--- Module 3/3.1 Directed Cycle/Solution.java
+++ Module 3/3.1 Directed Cycle/Solution.java
@@ -69,51 +69,38 @@ class DirectedCycle {
     public boolean hasCycle() {
         return cycle != null;
     }
-
-    // /**
-    //  * { function_description }
-    //  *
-    //  * @return     { description_of_the_return_value }
-    //  */
-    // public Iterable<Integer> cycle() {
-    //     return cycle;
-    // }
-
-
-    // // certify that digraph has a directed cycle if it reports one
-    // private boolean check() {
-
-    //     if (hasCycle()) {
-    //         // verify cycle
-    //         int first = -1, last = -1;
-    //         for (int v : cycle()) {
-    //             if (first == -1) first = v;
-    //             last = v;
-    //         }
-    //         if (first != last) {
-    //             System.err.printf("cycle begins with %d and ends with %d\n", first, last);
-    //             return false;
-    //         }
-    //     }
-
-
-    //     return true;
-    // }
 }
-class Solution {
-	public static void main(String[] args) {
+/**
+ * class Solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * reads input.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
 		int vertices = Integer.parseInt(input.nextLine());
 		int edges = Integer.parseInt(input.nextLine());
 		Digraph dg = new Digraph(vertices);
-		for(int i = 0; i<edges; i++) {
+		for (int i = 0; i < edges; i++) {
 			String[] tokens = input.nextLine().split(" ");
 			dg.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
 		}
 
 		DirectedCycle dc = new DirectedCycle(dg);
-		if(dc.hasCycle()) System.out.println("Cycle exists.");
-		else System.out.println("Cycle doesn't exists.");
+		if(dc.hasCycle()) {
+			System.out.println("Cycle exists.");
+		} else {
+			System.out.println("Cycle doesn't exists.");
+		}
 
 	}
 }
