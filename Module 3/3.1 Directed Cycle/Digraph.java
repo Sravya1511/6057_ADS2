5d18e7efb20b0d9d8ce86c7380843867588fba35
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-10-31 15:11:12 +0530

    "ads2_m3_a1_1: 5/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 3/3.1 Directed Cycle/Digraph.java
+++ Module 3/3.1 Directed Cycle/Digraph.java
@@ -1,12 +1,19 @@
-class Digraph {
-	private final int V;
-	private int E;
-	Bag<Integer>[] adj;
-	private int[] indegree;
+public class Digraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
 
+    private final int V;           // number of vertices in this digraph
+    private int E;                 // number of edges in this digraph
+    Bag<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+    private int[] indegree;        // indegree[v] = indegree of vertex v
 
+    /**
+     * Initializes an empty digraph with <em>V</em> vertices.
+     *
+     * @param  V the number of vertices
+     * @throws IllegalArgumentException if {@code V < 0}
+     */
     public Digraph(int V) {
-        // if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
         this.V = V;
         this.E = 0;
         indegree = new int[V];
@@ -16,14 +23,31 @@ class Digraph {
         }
     }
 
+    /**
+     * Returns the number of vertices in this digraph.
+     *
+     * @return the number of vertices in this digraph
+     */
     public int V() {
         return V;
     }
 
+    /**
+     * Returns the number of edges in this digraph.
+     *
+     * @return the number of edges in this digraph
+     */
     public int E() {
         return E;
     }
 
+    /**
+     * Adds the directed edge v→w to this digraph.
+     *
+     * @param  v the tail vertex
+     * @param  w the head vertex
+     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+     */
     public void addEdge(int v, int w) {
         // validateVertex(v);
         // validateVertex(w);
@@ -32,13 +56,16 @@ class Digraph {
         E++;
     }
 
-    public int outdegree(int v) {
+    /**
+     * Returns the vertices adjacent from vertex {@code v} in this digraph.
+     *
+     * @param  v the vertex
+     * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public Iterable<Integer> adj(int v) {
         // validateVertex(v);
-        return adj[v].size();
+        return adj[v];
     }
 
-    public int indegree(int v) {
-        // validateVertex(v);
-        return indegree[v];
-    }
 }
