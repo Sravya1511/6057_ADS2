2e40feaf1ae32a0cde377d30a4de82d02231b577
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-03 11:24:52 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 6/ADS-2Week-1/Solution.java
+++ Module 6/ADS-2Week-1/Solution.java
@@ -1,5 +1,55 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 class PageRank {
+	Digraph digraph;
+	int vertices;
+	double[] prValues;
+	int[] outlinks;
+    // Bag<Integer>[] adj;    // adj[v] = adjacency list for vertex v
+
+
+
+	PageRank(Digraph digraph, int vertices) {
+		this.digraph = digraph;
+		this.vertices = vertices;
+		prValues = new double[vertices];
+	    outlinks = new int[vertices];
+        // adj = (Bag<Integer>[]) new Bag[vertices];
+
+
+	}
+
+	public void initializePR(String[] incoming) {
+        for(int i = 0; i<vertices; i++) {
+        	outlinks[i] = digraph.outdegree(i);
+        	prValues[i] = 1.0/4;
+        }
+    // System.out.println(Arrays.toString(outlinks));
+    // System.out.println(Arrays.toString(incoming));
+    // System.out.println(Arrays.toString(prValues));
+
+
+
+
+	}
+
+	public void calculatePR() {
+         for(int i = 0; i<1000; i++) {
+
+        }
+	}
+
+	public String toString()  {
+		StringBuilder s = new StringBuilder();
+
+		for(int i = 0; i<vertices; i++) {
+			s.append(i+": "+prValues[i]+"\n");
+		}
+		return s.toString();
+	}
+
+
 
 }
 
@@ -22,17 +72,23 @@ public class Solution {
         Scanner input  = new Scanner(System.in);
         int vertices = Integer.parseInt(input.nextLine());
         Digraph digraph = new Digraph(vertices);
+        String[] incoming = new String[vertices];
 
         for(int j = 0; j<vertices; j++) {
         	// System.out.println(vertices);
-        	String [] tokens = input.nextLine().split(" ");
+        	String a = input.nextLine();
+        	incoming[j] = a;
+        	String [] tokens = a.split(" ");
         	for(int i = 1; i<tokens.length; i++) {
                 digraph.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
         	}
         }
 
         System.out.println(digraph);
-        System.out.println();
+        // System.out.println();
+        PageRank page = new PageRank(digraph, vertices);
+        page.initializePR(incoming);
+        System.out.println(page);
 
 
 		// print the page rank object
