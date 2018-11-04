c0e2bddad6192eaf291caf2b1017d1b59b74fc2d
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-04 23:27:44 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 4/Code Camp - WordNet/WordNet/Bag.java
+++ Module 4/Code Camp - WordNet/WordNet/Bag.java
@@ -1,132 +1,160 @@
-/******************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *  Dependencies: StdIn.java StdOut.java
- *
- *  A generic bag or multiset, implemented using a singly linked list.
- *
- *  % more tobe.txt 
- *  to be or not to - be - - that - - - is
- *
- *  % java Bag < tobe.txt
- *  size of bag = 14
- *  is
- *  -
- *  -
- *  -
- *  that
- *  -
- *  -
- *  be
- *  -
- *  to
- *  not
- *  or
- *  be
- *  to
- *
- ******************************************************************************/
-
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The {@code Bag} class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  This implementation uses a singly linked list with a static nested class Node.
- *  See {@link LinkedBag} for the version from the
- *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayBag} for a version that uses a resizing array.
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Item> the generic type of an item in this bag
+ * @param      <Item>  The item
  */
-public class Bag<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of bag
-    private int n;               // number of elements in bag
 
-    // helper linked list class
-    private static class Node<Item> {
+public class Bag<Item> implements Iterable<Item> {
+    /**
+     *  number of elements in bag.
+     */
+    private int num;
+    /**
+     * beginning of bag.
+     */
+    private Node first;
+
+    /**
+     * Class for node.
+     */
+
+    private class Node {
+        /**
+         * item of type node.
+         */
         private Item item;
-        private Node<Item> next;
+        /**
+         * Node of type node.
+         */
+        private Node next;
     }
 
     /**
-     * Initializes an empty bag.
+     * Constructs the object.
      */
+
     public Bag() {
         first = null;
-        n = 0;
+        num = 0;
     }
 
     /**
-     * Returns true if this bag is empty.
+     * Determines if empty.
+     * The time complexity is O(1).
      *
-     * @return {@code true} if this bag is empty;
-     *         {@code false} otherwise
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return first == null;
     }
 
     /**
-     * Returns the number of items in this bag.
+     * returns size of the bag.
+     * The time complexity is O(1).
      *
-     * @return the number of items in this bag
+     *
+     * @return     { int }
      */
     public int size() {
-        return n;
+        return num;
     }
 
     /**
-     * Adds the item to this bag.
+     * adds element into bag.
+     * The time complexity is O(1).
      *
-     * @param  item the item to add to this bag
+     *
+     * @param      item  The item
      */
-    public void add(Item item) {
-        Node<Item> oldfirst = first;
-        first = new Node<Item>();
+    public void add(final Item item) {
+        Node oldfirst = first;
+        first = new Node();
         first.item = item;
         first.next = oldfirst;
-        n++;
+        num++;
+    }
+    /**
+     * determines if there is vertex or not.
+     * The time complexity is O(N).
+     *
+     *
+     * @param      item  The item
+     *
+     * @return     { true or false }
+     */
+
+    public boolean contains(final Item item) {
+        Node n = first;
+        while (n != null) {
+            if (n.item == item) {
+                return true;
+            }
+            n = n.next;
+        }
+        return false;
     }
 
 
     /**
-     * Returns an iterator that iterates over the items in this bag in arbitrary order.
+     * Return an iterator that iterates over the items in the bag.
+     * The time complexity is O(N).
      *
-     * @return an iterator that iterates over the items in this bag in arbitrary order
+     *
+     * @return     { iterator }
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
+        return new ListIterator();
     }
+    /**
+     * Class for list iterator.
+     */
 
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
+    private class ListIterator implements Iterator<Item> {
+        /**
+         * Node.
+         */
+        private Node current = first;
+        /**
+         * Determines if it has next.
+         * The time complexity is O(1).
+         *
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext()  {
+            return current != null;
         }
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+        /**
+         * removes.
+         *The time complexity is O(1).
+         *
+         *
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * gives next node.
+         *The time complexity is O(1).
+         *
+         *
+         * @return     { item }
+         */
 
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                 throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
+
 }
+
+
+
+
