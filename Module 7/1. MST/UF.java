3bb25faeb68e03995df8306d6bc19a6f98cab2e5
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-05 19:51:04 +0530

    "ads2_m7_a1_1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 7/1. MST/UF.java
+++ Module 7/1. MST/UF.java
@@ -1,18 +1,7 @@
-/**
- * Class for uf.
- */
 public class UF {
-    /**
-     * parent array.
-     */
+
     private int[] parent;  // parent[i] = parent of i
-    /**
-     * rank array.
-     */
-    private byte[] rank;
-    /**
-     * count.
-     */
+    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
     private int count;     // number of components
 
     /**
@@ -21,8 +10,10 @@ public class UF {
      * component.
      *
      * @param  n the number of sites
+     * @throws IllegalArgumentException if {@code n < 0}
      */
-    public UF(final int n) {
+    public UF(int n) {
+        if (n < 0) throw new IllegalArgumentException();
         count = n;
         parent = new int[n];
         rank = new byte[n];
@@ -31,17 +22,18 @@ public class UF {
             rank[i] = 0;
         }
     }
+
     /**
-     * Returns the component identifier for the
-     * component containing site {@code p}.
+     * Returns the component identifier for the component containing site {@code p}.
+     *
      * @param  p the integer representing one site
-     * @return the component identifier for the
-     * component containing site {@code p}
+     * @return the component identifier for the component containing site {@code p}
+     * @throws IllegalArgumentException unless {@code 0 <= p < n}
      */
-    public int find(final int temp) {
-        int p = temp;
+    public int find(int p) {
+        validate(p);
         while (p != parent[p]) {
-            parent[p] = parent[parent[p]];
+            parent[p] = parent[parent[p]];    // path compression by halving
             p = parent[p];
         }
         return p;
@@ -61,11 +53,12 @@ public class UF {
      *
      * @param  p the integer representing one site
      * @param  q the integer representing the other site
-     * @return {@code true} if the two sites {@code p} and {@code q}
-     * are in the same component;
+     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
      *         {@code false} otherwise
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
-    public boolean connected(final int p, final int q) {
+    public boolean connected(int p, int q) {
         return find(p) == find(q);
     }
 
@@ -75,22 +68,40 @@ public class UF {
      *
      * @param  p the integer representing one site
      * @param  q the integer representing the other site
+     * @throws IllegalArgumentException unless
+     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
-    public void union(final int p, final int q) {
+    public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
-        if (rootP == rootQ) {
-            return;
-        }
+        if (rootP == rootQ) return;
+
         // make root of smaller rank point to root of larger rank
-        if (rank[rootP] < rank[rootQ]) {
-            parent[rootP] = rootQ;
-        } else if (rank[rootP] > rank[rootQ]) {
-            parent[rootQ] = rootP;
-        } else {
+        if      (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
+        else {
             parent[rootQ] = rootP;
             rank[rootP]++;
         }
         count--;
     }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    /**
+     * Reads in a an integer {@code n} and a sequence of pairs of integers
+     * (between {@code 0} and {@code n-1}) from standard input, where each integer
+     * in the pair represents some site;
+     * if the sites are in different components, merge the two components
+     * and print the pair to standard output.
+     *
+     * @param args the command-line arguments
+     */
+
 }
