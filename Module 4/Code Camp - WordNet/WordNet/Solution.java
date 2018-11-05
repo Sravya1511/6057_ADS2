67dba4081db90916b57e8e681b6ba0c6f33c9c09
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-05 09:42:00 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 4/Code Camp - WordNet/WordNet/Solution.java
+++ Module 4/Code Camp - WordNet/WordNet/Solution.java
@@ -15,27 +15,27 @@ final class Solution {
     /**
      * Constructs the object.
      */
-	private Solution() {
+    private Solution() {
 
-	}
+    }
     /**
      * creates object for digraph, SAP.
      *
      * @param      args  The arguments
      */
-		public static void main(final String[] args) {
-			Scanner input = new Scanner(System.in);
-			String x = input.nextLine();
-			HashMap<Integer, String[]> synset =
+        public static void main(final String[] args) {
+            Scanner input = new Scanner(System.in);
+            String x = input.nextLine();
+            HashMap<Integer, String[]> synset =
             new HashMap<Integer, String[]>();
-			HashMap<String, ArrayList<Integer>> synsetQueries =
+            HashMap<String, ArrayList<Integer>> synsetQueries =
             new HashMap<String, ArrayList<Integer>>();
 
-			try {
-			    Scanner scanner = new
+            try {
+                Scanner scanner = new
                 Scanner(new File("Files/" + x));
                 while (scanner.hasNextLine()) {
-            	   //  // System.out.println(scanner.nextLine());
+                   //  // System.out.println(scanner.nextLine());
                    String[] tokens = scanner.nextLine().split(",");
                    String[] words = tokens[1].split(" ");
                    synset.put(Integer.parseInt(tokens[0]), words);
@@ -43,79 +43,80 @@ final class Solution {
 
                 }
                 for (int i: synset.keySet()) {
-                	String[] keys = synset.get(i);
-                	for (int j = 0; j < keys.length; j++) {
-                		if (synsetQueries.containsKey(keys[j])) {
-                			ArrayList<Integer> intarr =
+                    String[] keys = synset.get(i);
+                    for (int j = 0; j < keys.length; j++) {
+                        if (synsetQueries.containsKey(keys[j])) {
+                            ArrayList<Integer> intarr =
                             synsetQueries.get(keys[j]);
-                			intarr.add(i);
-                			synsetQueries.put(keys[j], intarr);
-                		} else {
-                			ArrayList<Integer> indx =
+                            intarr.add(i);
+                            synsetQueries.put(keys[j], intarr);
+                        } else {
+                            ArrayList<Integer> indx =
                             new ArrayList<Integer>();
-                			indx.add(i);
-                			synsetQueries.put(keys[j], indx);
+                            indx.add(i);
+                            synsetQueries.put(keys[j], indx);
 
-                		}
+                        }
 
-                	}
+                    }
 
 
                 }
                 // System.out.println(synset);
                 // System.out.println(synsetQueries);
             } catch (FileNotFoundException e) {
-		    }
-			String y = input.nextLine();
-			Digraph digraph = new Digraph(synset.size());
+            }
+            String y = input.nextLine();
+            Digraph digraph = new Digraph(synset.size());
 
-		    try {
-			    Scanner scanner1 = new Scanner
+            try {
+                Scanner scanner1 = new Scanner
                 (new File("Files/" + y));
                 while (scanner1.hasNextLine()) {
-            	    String[] vertices = scanner1.nextLine().split(",");
-            	    for (int i = 1; i < vertices.length; i++) {
+                    String[] vertices = scanner1.nextLine().split(",");
+                    for (int i = 1; i < vertices.length; i++) {
                         digraph.addEdge(Integer.parseInt(vertices[0]),
-            	    	Integer.parseInt(vertices[i]));
-            	    }
+                        Integer.parseInt(vertices[i]));
+                    }
 
                 }
             } catch (FileNotFoundException e) {
-		    }
+            }
             String s = input.nextLine();
             DirectedCycle directedCycle = new DirectedCycle(digraph);
                 switch (s) {
-                	case "Graph":
-                	if (directedCycle.hasCycle()) {
+                    case "Graph":
+                    if (directedCycle.hasCycle()) {
 
-                		System.out.println("Cycle detected");
-                		    	return;
-                		    }
+                        System.out.println("Cycle detected");
+                                return;
+                            }
                     if (digraph.checkMultiple()) {
-                    	System.out.println("Multiple roots");
-                    	return;
+                        System.out.println("Multiple roots");
+                        return;
                     }
-                	System.out.println(digraph);
-                	case "Queries":
-                	while (input.hasNext()) {
-                		String[] tokens = input.nextLine().split(" ");
-                		try {
-                			if (tokens[0].equals("null")) {
-                			System.out.
+                    System.out.println(digraph);
+                    case "Queries":
+                    while (input.hasNext()) {
+                        String[] tokens = input.nextLine().split(" ");
+                        try {
+                            if (tokens[0].equals("null")) {
+                            System.out.
                             println("IllegalArgumentException");
-                			return;
-                		    }
-                		} catch (IllegalArgumentException e) {
-                			System.out.println(e.getMessage());
-                		}
-                		SAP sap = new SAP(digraph);
-                		sap.length(synsetQueries.get(tokens[0]),
+                            return;
+                            }
+                        } catch (IllegalArgumentException e) {
+                            System.out.println(e.getMessage());
+                        }
+                        SAP sap = new SAP(digraph);
+                        sap.length(synsetQueries.get(tokens[0]),
                             synsetQueries.get(tokens[1]), synset);
 
-                		}
+                        }
                     default:
                     break;
 
-                	}
-		}
-	}
+                    }
+        }
+    }
+
