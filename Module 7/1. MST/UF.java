de24c16ae15959bdc82f79457a86624878d7f25e
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-05 19:52:51 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 7/1. MST/UF.java
+++ Module 7/1. MST/UF.java
@@ -103,5 +103,16 @@ public class UF {
      *
      * @param args the command-line arguments
      */
-
+    // public static void main(String[] args) {
+    //     int n = StdIn.readInt();
+    //     UF uf = new UF(n);
+    //     while (!StdIn.isEmpty()) {
+    //         int p = StdIn.readInt();
+    //         int q = StdIn.readInt();
+    //         if (uf.connected(p, q)) continue;
+    //         uf.union(p, q);
+    //         StdOut.println(p + " " + q);
+    //     }
+    //     StdOut.println(uf.count() + " components");
+    // }
 }
