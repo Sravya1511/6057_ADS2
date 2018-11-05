3bb06b60e476a49cda8b93de9ace8d211c74a43a
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-05 09:16:52 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 4/Code Camp - WordNet/WordNet/Solution.java
+++ Module 4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,9 +1,9 @@
 import java.io.File;
-import java.io.IOException;
+// import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.ArrayList;
 
 /**
@@ -23,38 +23,43 @@ final class Solution {
      *
      * @param      args  The arguments
      */
-		public static void main(String[] args) {
+		public static void main(final String[] args) {
 			Scanner input = new Scanner(System.in);
 			String x = input.nextLine();
-			HashMap<Integer, String[]> synset = new HashMap<Integer, String[]>();
-			HashMap<String, ArrayList<Integer>> synsetQueries = new HashMap<String, ArrayList<Integer>>();
+			HashMap<Integer, String[]> synset =
+            new HashMap<Integer, String[]>();
+			HashMap<String, ArrayList<Integer>> synsetQueries =
+            new HashMap<String, ArrayList<Integer>>();
 
 			try {
-			    Scanner scanner = new Scanner(new File("Files/" + x));
+			    Scanner scanner = new
+                Scanner(new File("Files/" + x));
                 while (scanner.hasNextLine()) {
             	   //  // System.out.println(scanner.nextLine());
                    String[] tokens = scanner.nextLine().split(",");
                    String[] words = tokens[1].split(" ");
                    synset.put(Integer.parseInt(tokens[0]), words);
-                   // System.out.println(tokens[0]+"----"+Arrays.toString(words));
+
 
                 }
                 for (int i: synset.keySet()) {
                 	String[] keys = synset.get(i);
                 	for (int j = 0; j < keys.length; j++) {
-                		if(synsetQueries.containsKey(keys[j])) {
-                			ArrayList<Integer> intarr = synsetQueries.get(keys[j]);
+                		if (synsetQueries.containsKey(keys[j])) {
+                			ArrayList<Integer> intarr =
+                            synsetQueries.get(keys[j]);
                 			intarr.add(i);
                 			synsetQueries.put(keys[j], intarr);
                 		} else {
-                			ArrayList<Integer> indx = new ArrayList<Integer>();
+                			ArrayList<Integer> indx =
+                            new ArrayList<Integer>();
                 			indx.add(i);
                 			synsetQueries.put(keys[j], indx);
 
                 		}
 
                 	}
-                	// System.out.println(i+"----"+Arrays.toString(synset.get(i)));
+
 
                 }
                 // System.out.println(synset);
@@ -65,7 +70,8 @@ final class Solution {
 			Digraph digraph = new Digraph(synset.size());
 
 		    try {
-			    Scanner scanner1 = new Scanner(new File("Files/" + y));
+			    Scanner scanner1 = new Scanner
+                (new File("Files/" + y));
                 while (scanner1.hasNextLine()) {
             	    String[] vertices = scanner1.nextLine().split(",");
             	    for (int i = 1; i < vertices.length; i++) {
@@ -81,7 +87,7 @@ final class Solution {
                 switch (s) {
                 	case "Graph":
                 	if (directedCycle.hasCycle()) {
-                			// throw new IllegalArgumentException("IllegalArgumentException");
+
                 		System.out.println("Cycle detected");
                 		    	return;
                 		    }
@@ -95,16 +101,20 @@ final class Solution {
                 		String[] tokens = input.nextLine().split(" ");
                 		try {
                 			if (tokens[0].equals("null")) {
-                			System.out.println("IllegalArgumentException");
+                			System.out.
+                            println("IllegalArgumentException");
                 			return;
                 		    }
                 		} catch (IllegalArgumentException e) {
                 			System.out.println(e.getMessage());
                 		}
                 		SAP sap = new SAP(digraph);
-                		sap.length(synsetQueries.get(tokens[0]), synsetQueries.get(tokens[1]), synset);
+                		sap.length(synsetQueries.get(tokens[0]),
+                            synsetQueries.get(tokens[1]), synset);
 
                 		}
+                    default:
+                    break;
 
                 	}
 		}
