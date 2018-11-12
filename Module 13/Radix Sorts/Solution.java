a64621393ecb684ede80ad91739da8b49e017dfc
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-12 14:52:02 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 13/Radix Sorts/Solution.java
+++ Module 13/Radix Sorts/Solution.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class Solution{
-	public static void main(String[] args) {
+/**
+ * class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+	}
+	/**
+	 * reads input.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = Integer.parseInt(input.nextLine());
 		String[] array = new String[n];
-		for(int i = 0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			array[i] = input.nextLine();
 		}
 		Quick3string q3s = new Quick3string();
