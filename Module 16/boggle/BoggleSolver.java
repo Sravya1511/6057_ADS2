cf159df7601c19ea24e97e2018e240f232d38c89
bhaskaravsravya <bhaskaravsravya@msitprogram.net>
2018-11-18 19:45:03 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Module 16/boggle/BoggleSolver.java
+++ Module 16/boggle/BoggleSolver.java
@@ -1,69 +1,100 @@
 import java.util.HashSet;
-
+/**
+ * Class for boggle solver.
+ */
 public class BoggleSolver {
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+	/**
+	 * // Initializes the data structure using.
+	// the given array of strings as the dictionary.
+	// (You can assume each word in the dictionary.
+	//  contains only the uppercase letters A through Z.)
 	// Bag<String> bag =  new Bag<String>();
-	HashSet<String> bag = new HashSet<String>();
-	TriesST<Integer> triesST = new TriesST<Integer>();
+	 */
+	/**
+	 * bag - hashset
+	 */
 
+	HashSet<String> bag = new HashSet<String>();
+	/**
+	 * tries ST
+	 */
+	TriesST<Integer> triesST = new TriesST<Integer>();
+    /**
+     * Constructs the object.
+     *
+     * @param      dictionary  The dictionary
+     */
 	public BoggleSolver(String[] dictionary) {
 		int j = 0;
-		for(int i = 0; i<dictionary.length; i++) {
+		for (int i = 0; i < dictionary.length; i++) {
 			triesST.put(dictionary[i], j);
 			j++;
 		}
 	}
 
-	// Returns the set of all valid words in the given Boggle board, as an Iterable.
-	public Iterable<String> getAllValidWords(BoggleBoard board) {
+    /**
+     * Gets all valid words.
+     *
+     * @param      board  The board
+     *
+     * @return     All valid words.
+     */
+	public Iterable<String>
+	getAllValidWords(final BoggleBoard board) {
 		if(board == null) {
 			System.out.println("board is null");
 			return null;
 		}
 		int count = 0;
-		for(int i = 0; i<board.rows(); i++) {
-			for(int j = 0; j<board.cols(); j++)  {
-				boolean[][] marked = new boolean[board.rows()][board.cols()];
+		for (int i = 0; i < board.rows(); i++) {
+			for (int j = 0; j < board.cols(); j++)  {
+				boolean[][] marked = new
+				boolean[board.rows()][board.cols()];
 				validwords(board, i, j, "", marked);
 				count++;
 			}
 		}
-		// for(String g : bag) {
-		// 	System.out.println(g);
-		// }
-		// System.out.println(count);
 		return bag;
 	}
+	/**
+	 * valid words - dfs
+	 *
+	 * @param      board   The board
+	 * @param      i       { row }
+	 * @param      j       { column }
+	 * @param      word    The word
+	 * @param      marked  The marked
+	 */
 
-	public void validwords(BoggleBoard board, int i, int j, String word, boolean[][] marked) {
-		if(marked[i][j]) {
+	public void validwords(final BoggleBoard board, final int i,
+		final int j, String word, final boolean[][] marked) {
+		if (marked[i][j]) {
 			return;
 		}
 
-		if(!triesST.hasPrefix(word)) {
+		if (!triesST.hasPrefix(word)) {
 			return;
+		}
 
-	}
-		// if(word == "") {
 		    char a = board.getLetter(i, j);
 
-		    if(a == 'Q') {
+		    if (a == 'Q') {
 		    	word += "QU";
-		    }
-		    else {
+		    } else {
 		    	word += a;
 		    }
-		// }
-		if(word.length() > 2 && triesST.contains(word)) {
+
+		if (word.length() > 2
+			&& triesST.contains(word)) {
 			bag.add(word);
 		}
 	    marked[i][j] = true;
-		for(int di = -1; di<=1; di++) {
-			for(int dj = -1; dj<=1; dj++) {
-				int m = i+di;
-				int n = j+dj;
-				if(n >= 0 && n < board.cols() && m >= 0 && m <board.rows()) {
+		for (int di = -1; di <= 1; di++) {
+			for (int dj = -1; dj <= 1; dj++) {
+				int m = i + di;
+				int n = j + dj;
+				if (n >= 0 && n < board.cols()
+					&& m >= 0 && m < board.rows()) {
 					validwords(board, m, n, word, marked);
 				}
 			}
@@ -71,25 +102,36 @@ public class BoggleSolver {
 		marked[i][j] = false;
 	}
 
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A through Z.)
-	public int scoreOf(String word) {
-		if(!triesST.contains(word)) {
+	/**
+	 * returns scores of the words
+	 *
+	 * @param      word  The word
+	 *
+	 * @return     { scores - int }
+	 */
+	public int scoreOf(final String word) {
+		final int seven = 7;
+		final int eleven = 11;
+		final int five = 5;
+		final int three = 3;
+		final int six = 6;
+		final int four = 4;
+		if (!triesST.contains(word)) {
 			return 0;
 		}
 		int len = word.length();
-		if(len <= 2) {
+		if (len <= 2) {
 			return 0;
-		} else if(len <= 4) {
+		} else if (len <= four) {
 			return 1;
-		} else if(len == 5) {
+		} else if (len == five) {
 			return 2;
-		} else if(len == 6) {
-			return 3;
-		} else if(len == 7) {
-			return 5;
+		} else if (len == six) {
+			return three;
+		} else if (len == seven) {
+			return five;
 		} else {
-			return 11;
+			return eleven;
 		}
 	}
 }
